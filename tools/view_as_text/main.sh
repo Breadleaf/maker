4e6f5d89f940dd424e0247fadb664772915867ff
breadleaf <bkhnapa@gmail.com>
2022-06-28 08:09:39 +0000

    added new tool to check the output of converter easier

---
+++ tools/view_as_text/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script takes in a file that has been converted by converter and prints it
+# How the code will print in.
+
+if [ $# -ne 1 ]; then
+	echo -e "Invalid arg count: $#\nThe only argument is the path to the file\nex: ./view_as_text.sh ../example.tmp"
+	exit
+fi
+
+printf "$(cat $1)"
