b090a322809a4652aa6bf8a91310723f80a6f07b
breadleaf <bkhnapa@gmail.com>
2022-06-28 08:13:00 +0000

    fixed typos in tools/view_as_text/main.sh

--- tools/view_as_text/main.sh
+++ tools/view_as_text/main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This script takes in a file that has been converted by converter and prints it
-# How the code will print in.
+# How the code will print it.
 
 if [ $# -ne 1 ]; then
-	echo -e "Invalid arg count: $#\nThe only argument is the path to the file\nex: ./view_as_text.sh ../example.tmp"
+	echo -e "Invalid arg count: $#\nThe only argument is the path to the file\nex: ./main.sh ../example.tmp"
 	exit
 fi
 
